c39510996aa30925c5443b6764608149316455bb
aidanCQ <aidan.keay@quantinuum.com>
2023-10-31 13:55:03 +0000

    Initial auth implementation.

--- qnexus/client/client.py
+++ qnexus/client/client.py
@@ -1,8 +1,47 @@
 import httpx
 from ..config import get_config
-
+from ..consts import CREDS_FILE_PATH
+from typing import Dict, Any
+import os
+from urllib.request import Request
 
 config = get_config()
+
+
+def read_config_file(path: str) -> Dict[str, Any]:
+    """Read a config of key value pairs as a dict"""
+    env_vars = {}
+    homedir = os.path.expanduser("~")
+    with open(f"{homedir}/{path}", encoding="UTF-8") as myfile:
+        for line in myfile:
+            name, var = line.partition("=")[::2]
+            env_vars[name.strip()] = var.strip()
+    return env_vars
+
+
+def write_config_file(path: str, obj: Dict[str, Any]) -> None:
+    """Replace content in a config with key value pairs."""
+    homedir = os.path.expanduser("~")
+    f = open(f"{homedir}/{path}", encoding="UTF-8", mode="w")
+    for key, val in obj.items():
+        f.write(f"{key}={val}")
+        f.write("\n")
+    return None
+
+
+def refresh_cookies(cookies: httpx.Cookies) -> None:
+    refresh_response = httpx.Client(base_url=f"{config.url}/auth").post(
+        "/tokens/refresh",
+        cookies=cookies,
+    )
+    cookies.extract_cookies(response=refresh_response)
+    write_config_file(
+        CREDS_FILE_PATH,
+        {
+            "myqos_id": cookies.get("myqos_id"),
+            "myqos_oat": cookies.get("myqos_oat"),
+        },
+    )
 
 
 class AuthHandler(httpx.Auth):
@@ -10,32 +49,31 @@ class AuthHandler(httpx.Auth):
 
     requires_response_body = True
 
-    def __init__(self, refresh_token):
-        self.access_token: str = ""
-        self.refresh_token: str = refresh_token
-
     def auth_flow(self, request):
-        response = yield request
-        if response.status_code == 401:
-            # If the server issues a 401 response attempt a refresh,\
-            cookies = httpx.Cookies({"myqos_oat": self.refresh_token})
-            refresh_response = httpx.Client(base_url=f"{config.url}/auth").post(
-                "/tokens/refresh",
-                cookies=cookies,
-            )
-            if refresh_response.status_code == 401:
-                print("Your session has expired.")
-
-            cookies.extract_cookies(response=refresh_response)
-            self.access_token = cookies["myqos_id"]
-            request.headers["Cookie"] = f"myqos_id={cookies['myqos_id']}"
-            yield request
+        creds = read_config_file(CREDS_FILE_PATH)
+        cookies = httpx.Cookies(
+            {
+                "myqos_oat": creds.get("myqos_oat") or "",
+                "myqos_id": creds.get("myqos_id") or "",
+            }
+        )
+        cookies.set_cookie_header(request)
+        if cookies.get("myqos_id"):
+            response: httpx.Response = yield request
+            if response.status_code != 401:
+                return response
+        if cookies.get("myqos_oat"):
+            print("Refreshing...")
+            refresh_cookies(cookies)
+            cookies.set_cookie_header(request)
+            response: httpx.Response = yield request
+            if response.status_code != 401:
+                return response
+        raise Exception("Unauthenticated")
 
 
 config = get_config()
 nexus_client = httpx.Client(
     base_url=config.url,
-    auth=AuthHandler(
-        refresh_token="sfv0iycvv7n6zAzZsDYF-MX7dPeIgUr5W0wWxwkKuyQm81eSpGnPt48p0FL8V3njdBKjAupH394mmvV1U1JaXg"
-    ),
+    auth=AuthHandler(),
 )
--- qnexus/consts.py
+++ qnexus/consts.py
@@ -1,1 +1,2 @@
 CONFIG_FILE_NAME = "qnx.toml"
+CREDS_FILE_PATH = ".qnx/auth"
