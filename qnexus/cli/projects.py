2c8f7bbd2e1d24e20d4a5ed8e9a48192192da31d
John Children <john.children@cambridgequantum.com>
2024-05-24 03:14:48 +0100

    feat: Restore some cli functionality
    Not really necessary but was easy enough to get working again. Only
    tested manually however.

--- qnexus/cli/projects.py
+++ qnexus/cli/projects.py
@@ -1,18 +1,19 @@
 """CLI for qnexus."""
 
-# import click
-# from typing_extensions import Unpack
+import click
+from typing_extensions import Unpack
 
-# from ..client.projects import Params, ParamsDict
-# from ..client.projects import projects as _projects
-# from .utils import add_options_to_command, is_documented_by
+from qnexus.client.project import Params, ParamsDict
+from qnexus.client.project import get
 
-
-# @click.command()
-# @is_documented_by(_projects)
-# def projects(**kwargs: Unpack[ParamsDict]):
-#     """List all projects"""
-#     click.echo(_projects(**kwargs))
+from .utils import add_options_to_command, is_documented_by
 
 
-# add_options_to_command(projects, Params)
+@click.command()
+@is_documented_by(get)
+def projects(**kwargs: Unpack[ParamsDict]):
+    """List all projects"""
+    click.echo(get(**kwargs))
+
+
+add_options_to_command(projects, Params)
