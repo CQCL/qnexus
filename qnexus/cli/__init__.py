681ee2a3a45c74445b7f1097eefe4aff2257aabc
Vanya Eccles <vanya.eccles@quantinuum.com>
2024-06-05 11:19:23 +0100

    infra: remove old client CLI modules

--- qnexus/cli/__init__.py
+++ qnexus/cli/__init__.py
@@ -7,18 +7,24 @@ from .circuits import circuits
 from .jobs import jobs
 from .projects import projects
 
+<<<<<<< HEAD
 # from .utils import init, status
 
+=======
+>>>>>>> 11eb4d1 (infra: remove old client CLI modules)
 
 @click.group()
 def entrypoint():
     """Quantinuum Nexus API client."""
 
 
+<<<<<<< HEAD
 # entrypoint.add_command(init)
 entrypoint.add_command(circuits)
 # entrypoint.add_command(status)
 entrypoint.add_command(projects)
 entrypoint.add_command(jobs)
+=======
+>>>>>>> 11eb4d1 (infra: remove old client CLI modules)
 entrypoint.add_command(login)
 entrypoint.add_command(logout)
