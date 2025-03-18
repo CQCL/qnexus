b62b9e4bbebc38181c9909c1406d2c7967d8987b
Richard Morrison <richard.morrison@quantinuum.com>
2025-03-18 09:30:28 +0000

    Add a test for Hugr encoding/decoding stability

---
+++ tests/test_hugr.py
@@ -0,0 +1,21 @@
+import qnexus as qnx
+from pathlib import Path
+from hugr.package import Package
+
+
+def test_hugr_encode_decode() -> None:
+    """
+    Given a known-good binary-encoded HUGR in a file, test that it can be loaded
+    into a hugr.package.Package instance, then serialised and deserialised with
+    fidelity.
+
+    We expect this test to continue passing when client.hugr.ENVELOPE_CONFIG is
+    changed.
+    """
+    with open(Path(__file__).parent.resolve() / "data" / "hugr.dat", "rb") as fp:
+        data = fp.read()
+        package = Package.from_bytes(data)
+
+    encoded_package = qnx.hugr._encode_hugr(package)
+    decoded_package = qnx.hugr._decode_hugr(encoded_package)
+    assert encoded_package == qnx.hugr._encode_hugr(decoded_package)
