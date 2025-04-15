1f99ad5be543b03160216046383808fac7972d94
Will Angenent <will.angenent@quantinuum.com>
2025-04-15 16:46:57 +0100

    fix: Disable unused and broken jobs and projects CLI

--- qnexus/cli/__init__.py
+++ qnexus/cli/__init__.py
@@ -3,9 +3,8 @@
 import click
 
 from .auth import login, logout
-from .jobs import jobs
-from .projects import projects
-
+# from .jobs import jobs
+# from .projects import projects
 # from .utils import init, status
 
 
@@ -16,7 +15,7 @@ def entrypoint() -> None:
 
 # entrypoint.add_command(init)
 # entrypoint.add_command(status)
-entrypoint.add_command(projects)
-entrypoint.add_command(jobs)
+# entrypoint.add_command(projects)
+# entrypoint.add_command(jobs)
 entrypoint.add_command(login)
 entrypoint.add_command(logout)
