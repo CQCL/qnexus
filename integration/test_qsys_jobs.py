b2a5eae4b819d5d55d87a786b26da236ab2e6993
Sonia Pignorel <sonia.pignorel@quantinuum.com>
2025-05-13 21:47:22 +0100

    docs: Update CHANGELOG for v0.18.1

--- integration/test_qsys_jobs.py
+++ integration/test_qsys_jobs.py
@@ -38,9 +38,9 @@ def prepare_teleportation() -> Any:
         if measure(src):
             z(bob)
 
-        result("teleported", measure(bob)) # type: ignore
+        result("teleported", measure(bob))  # type: ignore
 
-    return main.compile() # type: ignore
+    return main.compile()  # type: ignore
 
 
 def test_guppy_execution(
