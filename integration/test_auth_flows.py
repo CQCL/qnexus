d705d2f3a112af5e8ddc1c8fd81f3006951c27c8
vanyae-cqc <vanya.eccles@quantinuum.com>
2025-05-13 18:37:24 +0100

    test: integration test for changing domain dynamically

--- integration/test_auth_flows.py
+++ integration/test_auth_flows.py
@@ -4,8 +4,11 @@ from io import StringIO
 from typing import Any
 
 import pytest
+from httpx import ConnectError
 
 import qnexus as qnx
+from qnexus.client import get_nexus_client
+from qnexus.client.auth import login_no_interaction
 from qnexus.client.utils import read_token
 from qnexus.config import get_config
 from qnexus.exceptions import AuthenticationError
@@ -56,3 +59,35 @@ def test_credential_login_full_flow(
 @pytest.mark.skip(reason="Not implemented")
 def test_device_code_flow_login_full_flow() -> None:
     """Test the flow for logging in with the browser."""
+
+
+def test_domain_switch(
+    monkeypatch: Any,
+) -> None:
+    """Set that we can reset the domain, login and not
+    for tokens/URL to be dynamically loaded."""
+
+    username = get_config().qa_user_email
+    pwd = get_config().qa_user_password
+
+    login_no_interaction(username, pwd)
+
+    qnx.users.get_self()
+
+    original_domain = get_config().domain
+
+    # fake domain will reset the client value
+    qnx.logout()
+    fake_domain = "fake_nexus.com"
+    monkeypatch.setenv("NEXUS_DOMAIN", fake_domain)
+    assert fake_domain in str(get_nexus_client(reload=True).base_url)
+
+    with pytest.raises(ConnectError):
+        qnx.users.get_self()
+
+    # setting it again will update the client without any restart required
+    monkeypatch.setenv("NEXUS_DOMAIN", original_domain)
+    login_no_interaction(username, pwd)
+    assert original_domain in str(get_nexus_client().base_url)
+
+    qnx.users.get_self()
