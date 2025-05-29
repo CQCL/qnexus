7b697d9289a28cc8bf004dc6ad02caef5a3da9ec
aidanCQ <aidan.keay@quantinuum.com>
2025-05-29 17:53:34 +0100

    Add qir crud.

--- qnexus/models/references.py
+++ qnexus/models/references.py
@@ -234,6 +234,39 @@ class HUGRRef(BaseRef):
             )
         )
 
+class QIRRef(BaseRef):
+    """Proxy object to a HUGR in Nexus."""
+
+    annotations: Annotations
+    project: ProjectRef
+    id: UUID
+    _contents: str | None = None
+    type: Literal["QIRRef"] = "QIRRef"
+
+    def download_hugr(self) -> Package:
+        """Get the HUGR Package of the original uploaded HUGR."""
+
+        if self._contents:
+            return self._contents
+
+        from qnexus.client.qir import _fetch_qir
+
+        self._contents = _fetch_qir(self)
+        return self._contents
+
+    def df(self) -> pd.DataFrame:
+        """Present in a pandas DataFrame."""
+        return self.annotations.df().join(
+            pd.DataFrame(
+                {
+                    "project": self.project.annotations.name,
+                    "id": self.id,
+                },
+                index=[0],
+            )
+        )
+
+
 
 class JobType(str, Enum):
     """Enum for a job's type."""
