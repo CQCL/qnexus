002809a4199ca489b7921fff5d35e6e74790cd58
aidanCQ <aidan.keay@quantinuum.com>
2025-08-12 14:24:48 +0100

    Revert reload changes.

--- qnexus/client/auth.py
+++ qnexus/client/auth.py
@@ -16,6 +16,7 @@ from qnexus.client import (
     VERSION,
     VERSION_HEADER,
     _check_version_headers,
+    get_nexus_client,
 )
 from qnexus.client.utils import consolidate_error, remove_token, write_token
 from qnexus.config import CONFIG
@@ -103,6 +104,7 @@ def login() -> None:
                 "access_token",
                 resp_json["access_token"],
             )
+            get_nexus_client(reload=True)
             # spinner.stop()
             print(
                 f"✅ Successfully logged in as {resp_json['email']} using the browser."
@@ -140,6 +142,7 @@ def logout() -> None:
     """Clear tokens from file system and the client."""
     remove_token("refresh_token")
     remove_token("access_token")
+    get_nexus_client(reload=True)
     print("Successfully logged out.")
 
 
@@ -184,6 +187,7 @@ def _request_tokens(user: EmailStr, pwd: str) -> None:
 
         write_token("refresh_token", myqos_oat)
         write_token("access_token", myqos_id)
+        get_nexus_client(reload=True)
 
         _check_version_headers(resp)
 
